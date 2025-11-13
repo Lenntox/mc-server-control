286e13d954f832b51374d09ce40570d79500c078
Jamie Bläsi <jbl140397@stud.gibb.ch>
2025-11-13 11:14:58 +0100

    fix test

--- src/app/mcstatus.service.spec.ts
+++ src/app/mcstatus.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { McstatusService } from './mcstatus.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('McstatusService', () => {
   let service: McstatusService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(McstatusService);
   });
 
